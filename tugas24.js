a61c6f1709d13aa3eaa286fc0a20a770cf6c468b
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-07 23:01:44 +0700

    Tugas 24 JS
    Tugas Niomic

---
+++ tugas24.js
@@ -0,0 +1,11 @@
+const tugas24 = () => {
+    var deret = [32,33,16,40,9,7,11,2,44,1,66];
+    deretString = deret.toString();
+    deretSort = deret.sort().toString();
+    deretRev = deret.reverse().toString();
+    console.log('Sebelumnya :',deretString);   
+    console.log('Ascending :',deretSort);
+    console.log('Descending :',deretRev);
+}
+
+tugas24();
